e9d3a362a533314256d9a90b1fca26e6cdd4d4c4
cjp-engr <engr.carmenjoy14@gmail.com>
2026-09-12 21:40:06 +0800

    add test case for simple cod checkout

--- elements/selectors/navBar.js
+++ elements/selectors/navBar.js
@@ -1,4 +1,7 @@
 // navBar.js
 output.navBar = {
+    homeNavTab: 'home_nav_tab',
     sellerNavTab: 'seller_nav_tab',
+    cartNavTab: 'cart_nav_tab',
+    ordersNavTab: 'orders_nav_tab',
 }
---
+++ elements/selectors/products.js
@@ -0,0 +1,4 @@
+output.products = {
+    searchField: 'search_field',
+    productList: 'product_list',
+}
