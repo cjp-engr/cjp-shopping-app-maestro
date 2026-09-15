cf23e2860e95025ec2b10094fb4a10469cb80ea1
cjp-engr <engr.carmenjoy14@gmail.com>
2026-09-15 11:19:46 +0800

    test: simple_new_credit_checkout_flow

---
+++ elements/selectors/checkout.js
@@ -0,0 +1,7 @@
+output.checkout = {
+    creditCard: {
+        expiryYearField: 'expiry_year_field',
+        expiryMonthField: 'expiry_month_field',
+        expiryItem: 'expiry_item_',
+    },
+}
